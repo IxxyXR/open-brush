0912b4ff86c1b070ce47d9ca44419f7e6f5ed8eb
Andy Baker <andy@andybak.net>
2021-02-20 16:57:09 +0000

    Folder structure

--- Assets/Polyhydra/Scripts/PolyhydraPopUpWindow.cs
+++ Assets/Polyhydra/Scripts/PolyhydraPopUpWindow.cs
@@ -20,15 +20,8 @@ using UnityEngine;
 
 namespace TiltBrush {
 
-public class PolyhydraPopUpWindow : PopUpWindow {
+public class PolyhydraPopUpWindow : PolyhydraPopUpWindowBase {
   
-  [SerializeField] private float m_ColorTransitionDuration;
-  
-  public GameObject ButtonPrefab;
-  
-  private float m_ColorTransitionValue;
-  private Material m_ColorBackground;
-
   override public void Init(GameObject rParent, string sText) {
     
     m_ColorBackground = m_Background.GetComponent<MeshRenderer>().material;
@@ -70,55 +63,6 @@ public class PolyhydraPopUpWindow : PopUpWindow {
       rButtonScript.SetDescriptionText(cats[i].ToString());
       rButtonScript.RegisterComponent();
     }
-  }
-  
-
-  override protected void BaseUpdate() {
-    base.BaseUpdate();
-
-    m_UIComponentManager.SetColor(Color.white);
-
-    // TODO: Make linear into smooth step!
-    if (m_ColorBackground &&
-        m_TransitionValue == m_TransitionDuration &&
-        m_ColorTransitionValue < m_ColorTransitionDuration) {
-      m_ColorTransitionValue += Time.deltaTime;
-      if (m_ColorTransitionValue > m_ColorTransitionDuration) {
-        m_ColorTransitionValue = m_ColorTransitionDuration;
-      }
-      float greyVal = 1 - m_ColorTransitionValue / m_ColorTransitionDuration;
-      m_ColorBackground.color = new Color(greyVal, greyVal, greyVal);
-    }
-  }
-
-  protected override void UpdateOpening() {
-    if (m_ColorBackground && m_TransitionValue == 0) {
-      m_ColorBackground.color = Color.white;
-    }
-    base.UpdateOpening();
-  }
-
-  protected override void UpdateClosing() {
-    if (m_ColorBackground) {
-      float greyVal = 1 - m_TransitionValue / m_TransitionDuration;
-      m_ColorBackground.color = new Color(greyVal, greyVal, greyVal);
-    }
-    base.UpdateClosing();
-  }
-
-  override public void UpdateUIComponents(Ray rCastRay, bool inputValid, Collider parentCollider) {
-    if (m_IsLongPressPopUp) {
-      // Don't bother updating the popup if we're a long press and we're closing.
-      if (m_CurrentState == State.Closing) {
-        return;
-      }
-      // If this is a long press popup and we're done holding the button down, get out.
-      if (m_CurrentState == State.Standard && !inputValid) {
-        RequestClose();
-      }
-    }
-
-    base.UpdateUIComponents(rCastRay, inputValid, parentCollider);
   }
 }
 }  // namespace TiltBrush
---
+++ Assets/Polyhydra/Scripts/PolyhydraPopUpWindowBase.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+namespace TiltBrush
+{
+    public class PolyhydraPopUpWindowBase : PopUpWindow
+    {
+        [SerializeField] protected float m_ColorTransitionDuration;
+        [SerializeField] protected GameObject ButtonPrefab;
+  
+        protected float m_ColorTransitionValue;
+        protected Material m_ColorBackground;
+
+        override protected void BaseUpdate() {
+            base.BaseUpdate();
+
+            m_UIComponentManager.SetColor(Color.white);
+
+            // TODO: Make linear into smooth step!
+            if (m_ColorBackground &&
+                m_TransitionValue == m_TransitionDuration &&
+                m_ColorTransitionValue < m_ColorTransitionDuration) {
+                m_ColorTransitionValue += Time.deltaTime;
+                if (m_ColorTransitionValue > m_ColorTransitionDuration) {
+                    m_ColorTransitionValue = m_ColorTransitionDuration;
+                }
+                float greyVal = 1 - m_ColorTransitionValue / m_ColorTransitionDuration;
+                m_ColorBackground.color = new Color(greyVal, greyVal, greyVal);
+            }
+        }
+
+        protected override void UpdateOpening() {
+            if (m_ColorBackground && m_TransitionValue == 0) {
+                m_ColorBackground.color = Color.white;
+            }
+            base.UpdateOpening();
+        }
+
+        protected override void UpdateClosing() {
+            if (m_ColorBackground) {
+                float greyVal = 1 - m_TransitionValue / m_TransitionDuration;
+                m_ColorBackground.color = new Color(greyVal, greyVal, greyVal);
+            }
+            base.UpdateClosing();
+        }
+
+        override public void UpdateUIComponents(Ray rCastRay, bool inputValid, Collider parentCollider) {
+            if (m_IsLongPressPopUp) {
+                // Don't bother updating the popup if we're a long press and we're closing.
+                if (m_CurrentState == State.Closing) {
+                    return;
+                }
+                // If this is a long press popup and we're done holding the button down, get out.
+                if (m_CurrentState == State.Standard && !inputValid) {
+                    RequestClose();
+                }
+            }
+
+            base.UpdateUIComponents(rCastRay, inputValid, parentCollider);
+        }
+    }
+}
