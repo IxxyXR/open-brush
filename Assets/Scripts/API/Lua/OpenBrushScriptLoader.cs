07f4a030c3f12d8a2b3be59b300d061758d5e0b4
Andy Baker <andy@andybak.net>
2023-11-30 10:01:01 +0000

    let's skip all attempts to read from disk for now
    [CI BUILD]

--- Assets/Scripts/API/Lua/OpenBrushScriptLoader.cs
+++ Assets/Scripts/API/Lua/OpenBrushScriptLoader.cs
@@ -23,12 +23,39 @@ namespace TiltBrush
     {
         public override bool ScriptFileExists(string name)
         {
+            return true;
             LuaManager.LogLuaMessage($"ScriptFileExists: {name}? {File.Exists(name)}");
             return File.Exists(name);
         }
 
         public override object LoadFile(string file, Table globalContext)
         {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(@"local symmetryHueShift =  {}
+if not Parameters then Parameters = {} end
+
+Parameters[""hueShiftFrequency""] = {label=""Hue Shift Frequency"", type=""float"", min=0.1, max=6, default=1}
+Parameters[""hueShiftAmount""] = {label=""Hue Shift Amount"", type=""float"", min=0, max=1, default=0.3}
+
+function symmetryHueShift.generate(copies, initialHsv)
+	Symmetry.ClearColors()
+	if hueShiftAmount > 0 then
+		for i = 0, copies - 1 do
+			t = i / copies
+			newHue = Waveform:Triangle(t, hueShiftFrequency) * hueShiftAmount
+			newColor = Color.HsvToRgb(initialHsv.x + newHue, initialHsv.y, initialHsv.z)
+			Symmetry.AddColor(newColor)
+		end
+		Brush.ForceNewStroke()
+	end
+end
+return symmetryHueShift
+");
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+
             FileStream result = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
             LuaManager.LogLuaMessage($"LoadFile: {file} CanRead? {result.CanRead}");
             return result;
